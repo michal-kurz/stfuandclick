0e95c99edb4b8045f709eea84947ccec9d2707b2
Michal Kurz <michalk@grappes.cz>
2020-10-17 22:58:52 +0200

    add reponame to router match to fix gh-pages (but break dev build)

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,15 +11,3 @@ exports.onCreatePage = async ({ page, actions }) => {
     createPage(page)
   }
 }
-
-exports.createPages = ({ actions }) => {
-  const { createRedirect } = actions
-
-  createRedirect({
-    fromPath: '/',
-    toPath: '/app',
-    redirectInBrowser: true,
-    isPermanent: true,
-    noThrow: true,
-  })
-}
--- src/utils/internalRouting.ts
+++ src/utils/internalRouting.ts
@@ -1,4 +1,4 @@
-const BASE_URL = '/app'
+export const BASE_URL = 'https://michal-kurz.github.io/stfuandclick'
 
 // eslint-disable-next-line import/prefer-default-export
-export const getTeamPath = (teamName: string) => `${BASE_URL}/team/${teamName}`
+export const getTeamPath = (teamName: string) => `${BASE_URL}/app/team/${teamName}`
