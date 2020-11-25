02235e58f19d7cc9de71d1c8fd60c94883c172d2
ErnstHess <michal.w.kurz@gmail.com>
2020-11-25 22:24:23 +0100

    attempt netlify redirect fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,13 @@
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
-// exports.onCreatePage = async ({ page, actions }) => {
-//   const { createPage } = actions
-//   // Only update the `/app` page.
-//   if (page.path.match(/^\/app/)) {
-//     // page.matchPath is a special key that's used for matching pages
-//     // with corresponding routes only on the client.
-//     page.matchPath = '/app/*' // eslint-disable-line no-param-reassign
-//     // Update the page.
-//     createPage(page)
-//   }
-// }
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+  // Only update the `/app` page.
+  if (page.path.match(/^\/app/)) {
+    // page.matchPath is a special key that's used for matching pages
+    // with corresponding routes only on the client.
+    page.matchPath = '/app/*' // eslint-disable-line no-param-reassign
+    // Update the page.
+    createPage(page)
+  }
+}
